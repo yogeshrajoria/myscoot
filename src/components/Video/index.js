a585781934d1d0dc0a919c2990b9686b9562f878
Yogesh Rajoria <yogeshmay30@gmail.com>
2020-01-08 01:44:22 +0530

    [UPDATE]: video controls visible.

--- src/components/Video/index.js
+++ src/components/Video/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Video(props) {
   return (
-    <video className={props.className} loop={true} 
+    <video className={props.className} loop={true} controls={true}
       src="https://storage.coverr.co/videos/Fire-Snow?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcHBJZCI6IjExNDMyN0NEOTRCMUFCMTFERTE3IiwiaWF0IjoxNTc4Mzk1MDM5fQ.idR1zOi7fr9mVWCCW_pRLVcNWcn2qt98pjcsd06g7qo" 
       poster="https://storage.coverr.co/posters/Fire-Snow">
       </video>
